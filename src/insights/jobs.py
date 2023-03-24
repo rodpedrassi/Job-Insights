9dc1df97c1719419ad274b54fe1a3e4f3f53e104
rodpedrassi <rodpedrassi@gmail.com>
2023-03-24 16:01:12 -0300

    feat: Adiciona função que permite ler arquivos csv retornando uma lista de dicionarios

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,23 +1,13 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    print('a')
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    with open(path, encoding="utf-8") as csvfile:
+        jobs = list(csv.DictReader(csvfile))
+        return jobs
 
 
 def get_unique_job_types(path: str) -> List[str]:
@@ -54,3 +44,8 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
         List of jobs with provided job_type
     """
     raise NotImplementedError
+
+
+if __name__ == "__main__":
+    jobs = read("data/jobs.csv")
+    print(jobs)
