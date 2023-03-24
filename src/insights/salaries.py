4f90e9d5042f34ed8de16ac6ec60ecf351491025
rodpedrassi <rodpedrassi@gmail.com>
2023-03-24 18:35:26 -0300

    feat: Adiciona função auxiliar que busca por erros para encontrar jobs por faixa salarial

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -24,29 +24,20 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    try:
+        min_salary = int(job["min_salary"])
+        max_salary = int(job["max_salary"])
+        salary = int(salary)
+        # Quando tem erro de tipos 1 / '2'
+    except TypeError:
+        raise ValueError
+        # Erro ao acessar uma chave que não existe (caso job venha sem as keys)
+    except KeyError:
+        raise ValueError
+    else:
+        if min_salary > max_salary:
+            raise ValueError
+        return min_salary <= salary <= max_salary
 
 
 def filter_by_salary_range(
@@ -74,4 +65,4 @@ if __name__ == "__main__":
     minSalary = get_min_salary("data/jobs.csv")
     print(minSalary)
     a = [1, 2, 3, 4]
-    print(min(a))
+    print("123".isnumeric())
