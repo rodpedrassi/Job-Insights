7af025ac8376a40d109ce4f290a4409591799b69
rodpedrassi <rodpedrassi@gmail.com>
2023-03-29 14:59:35 -0300

    feat: adiciona o test da função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,17 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+import pytest
+from unittest.mock import mock_open, patch
 
 
-def test_counter():
-    pass
+@pytest.fixture
+def file():
+    return """
+    teste
+    teste
+    teste
+    """
+
+
+def test_words_counter(file):
+    with patch("builtins.open", mock_open(read_data=file)):
+        assert count_ocurrences("path_mock", "teste") == 3
